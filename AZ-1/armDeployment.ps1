a54c13bb0ddc5fb68f3f2b15fdea4a0d453bff17
ShezzyBear <109043936+ShezzyBear@users.noreply.github.com>
2022-07-16 12:43:48 -0400

    Create armDeployment.ps1
    ARM deployment PowerShell code

---
+++ AZ-1/armDeployment.ps1
@@ -0,0 +1,15 @@
+$today=Get-Date -Format "MM-dd-yyyy"
+$deploymentName="addSkuParameter-"+"$today"
+New-AzResourceGroupDeployment `
+  -Name $deploymentName `
+  -TemplateFile $templateFile `
+  -storageName az104armtemptest `
+  -storageSKU Standard_GRS
+
+  $today=Get-Date -Format "MM-dd-yyyy"
+$deploymentName="addOutputs-"+"$today"
+New-AzResourceGroupDeployment `
+  -Name $deploymentName `
+  -TemplateFile $templateFile `
+  -storageName az104armtemptest2 `
+  -storageSKU Standard_LRS
